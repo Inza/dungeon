0cb926f3290087e33c73f950470a196e770b648f
Prelang Builder <builder@prelang.com>
2015-09-25 18:26:33 +0000

    Defined validation for attribute 'invoicing_company_website' on model 'Team'

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
   has_many :team_memberships
   validates_formatting_of :invoicing_email, using: :email
+  validates_formatting_of :invoicing_company_website, using: :url
 end
