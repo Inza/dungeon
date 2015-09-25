7efc7a964c7d6874c41c8234d33635fa9a2ca0ba
Prelang Builder <builder@prelang.com>
2015-09-25 18:26:38 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Invoice' to allow for create, update

--- app/admin/invoice.rb
+++ app/admin/invoice.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Invoice do
+  permit_params :state, :state_changed_at, :cost, :due_date
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
