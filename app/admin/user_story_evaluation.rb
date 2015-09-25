bd64a301b7a2707540f383f8329d9045c2fa2414
Prelang Builder <builder@prelang.com>
2015-09-25 18:26:47 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'UserStoryEvaluation' to allow for create, update

--- app/admin/user_story_evaluation.rb
+++ app/admin/user_story_evaluation.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register UserStoryEvaluation do
+  permit_params :points, :cost
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
